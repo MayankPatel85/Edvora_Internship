a958308b92c5b6cece7ebbc7ce5535e8ccd7d920
Mayank Patel <patelmayank8485@gmail.com>
2022-06-10 18:24:47 +0530

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by MΔΨΔΠҜ PΔTΣL on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
